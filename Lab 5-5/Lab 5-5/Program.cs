d9040e3730b7c024eb68f7635fc15159dd2de33f
Gloria Moreno <gloria.moreno@utp.ac.pa>
2025-09-22 19:42:32 -0500

    Lab5- Arreglos en C#

---
+++ Lab 5-5/Lab 5-5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Estudiante
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var estudiantes = new List<Estudiante>
+        {
+            new Estudiante { Nombre = "Ana", Edad = 12 },
+            new Estudiante { Nombre = "Juan", Edad = 10 },
+            new Estudiante { Nombre = "Sofia", Edad = 11 }
+        };
+
+        foreach (var estudiante in estudiantes)
+        {
+            Console.WriteLine($"Nombre: {estudiante.Nombre}, Edad: {estudiante.Edad}");
+        }
+    }
+}
