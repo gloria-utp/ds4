9bc363777eab9961ff7a8ddd09f76fe4245bf868
Gloria Moreno <gloria.moreno@utp.ac.pa>
2025-09-29 20:10:06 -0500

    Laboratorio # 8

---
+++ Laboratorio 8/Lab 8-7/Lab 8-7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Corrio la aplicacion");
+
+    }
+}
+
+class ClaseBase
+{
+    public void test()
+    {
+    }
+
+    public void moreTesting()
+    {
+    }
+}
+
+class ClaseHijo : ClaseBase
+{
+}
